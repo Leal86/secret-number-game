5e4f4d5efe275af9f9c99dfa75a21a374c4c69a2
Alexsander Leal <alexsander.leal86@gmail.com>
2024-05-01 14:11:19 +0100

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ console.log(numeroSecreto)
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
-    responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.2});
+    responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.1});
 }
 
 
